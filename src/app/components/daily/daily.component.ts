f0ce47be2755a655883a8f3a73417259051d748f
john harkins <johnleoharkins3@gmail.com>
2021-10-05 20:45:33 -0500

    yeah

--- src/app/components/daily/daily.component.ts
+++ src/app/components/daily/daily.component.ts
@@ -3,8 +3,8 @@ import * as dateFns from 'date-fns';
 
 import {Observable, of} from "rxjs";
 import {MatDialog, MatDialogClose, MatDialogConfig, MAT_DIALOG_DATA} from "@angular/material/dialog";
-import {shiftsMock} from "../../data.mock";
-import {map} from "rxjs/operators";
+import {shiftsMock, shiftsMock2} from "../../data.mock";
+import {concatMap, map} from "rxjs/operators";
 @Component({
   selector: 'app-daily',
   templateUrl: './daily.component.html',
@@ -13,6 +13,10 @@ import {map} from "rxjs/operators";
 export class DailyComponent implements OnInit {
   currentFormattedDate: Observable<any>;
   shiftsMock$: Observable<any> = of(shiftsMock);
+  shiftsMock2$: Observable<any> = of(shiftsMock2);
+  startOfWeekDate$: Observable<any> = new Observable<any>();
+
+  weeklyData$: Observable<any> = new Observable<any>()
 
   constructor(private matDialog: MatDialog) {
     this.currentFormattedDate = of(this.processCurrentDate());
@@ -20,24 +24,60 @@ export class DailyComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log(this.getRelativeTimeFormattedString(shiftsMock[0].timeOut))
+    // console.log(this.getRelativeTimeFormattedString(shiftsMock[0].timeOut))
+    if(this.currentFormattedDate == null){}else{
+      this.startOfWeekDate$ = this.getStartOfWeek();
+      this.weeklyData$ = this.processShiftData(this.shiftsMock2$)
+    }
 
   }
 
-  processCurrentWeek(){
-    this.currentFormattedDate.pipe(
+  getStartOfWeek(): Observable<any>{
+    return this.currentFormattedDate.pipe(
       map(formattedDate => {
+        console.log(formattedDate)
+        let startOfWeek: any;
         if(formattedDate.currentDayOfWeek == 0){
-
+          startOfWeek = formattedDate.currentDate;
         }else{
-          dateFns
+          startOfWeek = dateFns.previousSunday(formattedDate.currentDate)
         }
+        console.log(startOfWeek)
+        return startOfWeek;
+      })
+    )
+  }
+
+  processShiftData(shiftData: Observable<any>){
+    return this.startOfWeekDate$.pipe(
+      concatMap((sunday) => {
+        console.log(sunday)
+        let weekDates: Date[] = dateFns.eachDayOfInterval({start: sunday, end: dateFns.addDays(sunday, 6)});
+        let weekDatesKeyFormat: string[] = [];
+        weekDates.forEach((date) => {
+          let month = dateFns.getMonth(date)
+          let day = dateFns.getDate(date)
+          let year = dateFns.getYear(date)
+          console.log(month)
+          weekDatesKeyFormat.push(dateFns.lightFormat(date, 'MM-dd-yyyy'));
+        })
+        console.log(weekDatesKeyFormat)
+        return shiftData.pipe(
+          map((shiftsObject: any) => {
+            let shiftsInInterval: any[] = [];
+            weekDatesKeyFormat.forEach((dateKey) => {
+              shiftsInInterval = [...shiftsInInterval ,shiftsObject.dateKey]
+            })
+            console.log(shiftsInInterval)
+            return shiftsInInterval;
+          })
+        )
       })
     )
   }
 
   processCurrentDate(){
-    let currentDate = new Date();
+    let currentDate = new Date('July 13, 2021 00:00:0000');
     let currentMonth = dateFns.getMonth(currentDate);
     let currentDayOfWeek = dateFns.getDay(currentDate);
     let currentHour = dateFns.getHours(currentDate);
@@ -45,6 +85,7 @@ export class DailyComponent implements OnInit {
     let dateOfMonth = dateFns.getDate(currentDate);
     let currentYear = dateFns.getYear(currentDate);
     return {
+      currentDate,
       currentMonth,
       currentDayOfWeek,
       currentHour,
--- src/app/data.mock.ts
+++ src/app/data.mock.ts
@@ -25,4 +25,26 @@ export const shiftsMock = [
 
 ]
 
+export const shiftsMock2 = {
+  '07-13-2021':[{
+    project: 'Warehouse test this one is a long project',
+    jobCode: '1022',
+    payCode: 'N',
+    timeIn: new Date("Tue Jul 13 2021 20:55:55 GMT-0500"),
+    timeInFormatted: '8:55pm',
+    timeOut: new Date("Tue Jul 14 2021 05:55:55 GMT-0500"),
+    timeOutFormatted: '5:55am',
+    description: 'scrape ,primer, paint doors, area set up, tape and take down for operational during day',
+    totalHours: 8
+  },
+  {
+    project: 'Warehouse',
+    jobCode: '1022',
+    payCode: 'N',
+    timeIn: new Date("Tue Jul 15 2021 04:55:55 GMT-0500"),
+    timeOut: new Date("Tue Jul 15 2021 02:55:55 GMT-0500"),
+    description: 'scaffolding, paint walls, paint railings,',
+    totalHours: 8
+  }]
+}
 
